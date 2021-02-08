99c14f5c894a7c32214f96244b60803d177430aa
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-08 12:48:25 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email,           uniqueness: true
+  validates :nickname,        presence: true
+  validates :last_name_full,  presence: true
+  validates :first_name_full, presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday,        presence: true
 end
