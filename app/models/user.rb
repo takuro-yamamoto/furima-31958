32ffe0d25a4c6ac6838cc01582cef44f6992ac8b
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-08 14:38:49 +0900

    userモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email,           uniqueness: true
   validates :nickname,        presence: true
-  validates :last_name_full,  presence: true
-  validates :first_name_full, presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_full
+    validates :last_name_full
+  end
+  with_options presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birthday,        presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
