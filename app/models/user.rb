93b575af3647fd138c74af56ca247ed6020dbb6e
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-09 21:01:11 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,        presence: true
+  validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name_full
     validates :last_name_full
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday,        presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.build(:user)
     end
-    
+
     context 'ユーザー登録ができる時' do
       it '必要な情報を適切に入力すると、新規登録/ログインができる' do
         expect(@user).to be_valid
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
     end
     context 'ユーザー登録ができない時' do
       it 'ニックネームがないと登録できない' do
-        @user.nickname = nil 
+        @user.nickname = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
@@ -34,16 +34,16 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'メールアドレスは、@を含まないと登録できない' do
-        @user.email = "test.jp"
+        @user.email = 'test.jp'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'パスワードがないと登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      
+
       it 'passwordが5文字以下であれば登録できない' do
         @user.password = 'a2345'
         @user.password_confirmation = 'a2345'
@@ -54,11 +54,11 @@ RSpec.describe User, type: :model do
         @user.password = '123456'
         @user.password_confirmation = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
       it 'パスワードは、確認用のパスワードが空では登録できない' do
         @user.password = 'a23456'
-        @user.password_confirmation = ""
+        @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
@@ -79,14 +79,14 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name full can't be blank")
       end
       it 'ユーザー本名の名前は、全角（漢字・ひらがな・カタカナ）でないと登録できない' do
-        @user.first_name_full = "aaa1"
+        @user.first_name_full = 'aaa1'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name full 全角文字を使用してください")
+        expect(@user.errors.full_messages).to include('First name full 全角文字を使用してください')
       end
       it 'ユーザー本名の名字は、全角（漢字・ひらがな・カタカナ）でないと登録できない' do
         @user.last_name_full = 'aaa1'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name full 全角文字を使用してください")
+        expect(@user.errors.full_messages).to include('Last name full 全角文字を使用してください')
       end
       it 'ユーザー本名のフリガナは、名字がないと登録できない' do
         @user.last_name_kana = nil
@@ -99,34 +99,32 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it 'ユーザー本名のフリガナの名前は、全角（カタカナ）で入力しないと登録できない' do
-        @user.first_name_kana = "1aあ漢字"
+        @user.first_name_kana = '1aあ漢字'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana 全角カナ文字を使用してください")
+        expect(@user.errors.full_messages).to include('First name kana 全角カナ文字を使用してください')
       end
       it 'ユーザー本名のフリガナの名字は、全角（カタカナ）で入力しないと登録できない' do
-        @user.last_name_kana = "a1あ漢字"
+        @user.last_name_kana = 'a1あ漢字'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana 全角カナ文字を使用してください")
+        expect(@user.errors.full_messages).to include('Last name kana 全角カナ文字を使用してください')
       end
-      it '生年月日がないと登録できない'do
-      @user.birthday = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      it '生年月日がないと登録できない' do
+        @user.birthday = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
-      it 'パスワードは英語のみでは登録できないこと'do
-      @user.password = "abcdefg"
-      @user.password_confirmation = @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      it 'パスワードは英語のみでは登録できないこと' do
+        @user.password = 'abcdefg'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
-      it 'パスワードは全角では登録できないこと'do
-      @user.password = "あああああああああ"
-      @user.password_confirmation = @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      it 'パスワードは全角では登録できないこと' do
+        @user.password = 'あああああああああ'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
     end
   end
 end
-
-
