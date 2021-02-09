bd04096102254183a159ab19a192a5a49d976f6e
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-09 23:32:44 +0900

    itemモデルのバリデーション設定、activehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition 
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+  validates :name, :description, :price, presence: true
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
 end
