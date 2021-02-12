37f4d2e3f5994fad2f4522fc4327f6a1e73f868d
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-12 16:08:44 +0900

    itemコントローラーのset_item内の不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update,:destroy, :move_to_index]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -33,6 +33,7 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @item.destroy
     redirect_to root_path
