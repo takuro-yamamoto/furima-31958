7b3ab807c48cb100f67f9679be1d3e57ea8596a1
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-12 13:10:43 +0900

    itemコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update,:move_to_index]
   before_action :move_to_index, only: [:edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -41,8 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path if current_user.id != item.user.id
+    redirect_to root_path if current_user.id != @item.user.id
   end
 
   def set_item
