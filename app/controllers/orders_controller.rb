d177bbe567abe820f54e1d726a025092709f2863
takuro-yamamoto <taku50lax@outlook.jp>
2021-03-28 14:27:24 +0900

    出品者・出品者以外にかかわらず、ログイン状態のユーザーが、URLを直接入力して売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user.id
+    redirect_to root_path if current_user.id == @item.user.id || @item.order
   end
 end
