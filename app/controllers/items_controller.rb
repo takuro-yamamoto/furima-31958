09e3260dcb4c46b163459f9a670c474b1395ac4a
takuro-yamamoto <taku50lax@outlook.jp>
2021-03-29 18:39:21 +0900

    出品者・出品者以外にかかわらず、ログイン状態のユーザーが、URLを直接入力して売却済み商品の商品情報編集ページに遷移できないこと

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id != @item.user.id
+    redirect_to root_path if current_user.id != @item.user.id || @item.order
   end
 
   def set_item
